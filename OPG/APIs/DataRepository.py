7800bf29239b34fec92b92fc16705f213e4cede0
Carlos Fernandez-Basso <cjferba@gmail.com>
2017-11-29 17:02:27 +0000

    connect pizarra

--- OPG/APIs/DataRepository.py
+++ OPG/APIs/DataRepository.py
@@ -3,6 +3,53 @@ import pandas as pd
 import datetime
 import pytz
 import dateutil.parser
+from pathlib import Path
+import httplib2 as http
+from os import walk
+import shutil, os
+import datetime
+
+def SendToBlackBoard(Lista,pathJson,token='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJVc2VyIjoiT1BHIiwiUGlsb3QiOiJGQVJPIiwiZXhwIjoxNTE5MTMxOTk2LCJpYXQiOjE0ODc1OTU5OTZ9.ZJfXJrebFV54VttkPKjdbIYplJzEjkF54aGJek2MukQ'):
+    for i in Lista:
+        pathLista = os.path.join(dir, pathJson + i)
+        try:
+            from urlparse import urlparse
+        except ImportError:
+            from urllib.parse import urlparse
+
+        headers = {
+            'Content-Type': 'application/json',
+            'x-access-token': token
+        }
+
+        uri = 'http://citic-ps9.ugr.es:8099'
+        path = '/project/EnergyInTime/OPG/FaroAirport/setpoints/store-plan'
+
+        '''
+        data = {}
+        data['key'] = 'value'
+        json_data = json.dumps(data)
+        body = json.dumps(data);
+        '''
+
+        target = urlparse(uri + path)
+        method = 'POST'
+        import json
+        from pprint import pprint
+
+        with open(pathLista) as data_file:
+            data = json.load(data_file)
+
+        body = data
+        h = http.Http()
+
+        response, content = h.request(
+            target.geturl(),
+            method,
+            json.dumps(body),
+            headers)
+
+        print(content)
 
 
 class DRepositoryAPI:
@@ -58,12 +105,15 @@ class DRepositoryAPI:
 
         """Date to Timestamp"""
         Start = Timestamp(dateutil.parser.parse(DateStart))
-        print(Start)
         End = Timestamp(dateutil.parser.parse(DateEnd))
-        print(End)
         """Query"""
         self.__conn.select_collection(collect=Building)
-        cursor = self.__conn.collection.find({"ID_Sensor": {"$in": Sensors}, "timestamp": {"$gte": Start, "$lte": End}})
+        if len(Sensors)==1:
+            cursor = self.__conn.collection.find(
+                {"ID_Sensor": Sensors, "timestamp": {"$gte": Start, "$lte": End}})
+        else:
+            cursor = self.__conn.collection.find(
+                {"ID_Sensor": {"$in": Sensors}, "timestamp": {"$gte": Start, "$lte": End}})
         df = pd.DataFrame(list(cursor))
         """ Read from Mongo and Store into DataFrame """
         # Delete the _id
@@ -84,9 +134,12 @@ class DRepositoryAPI:
             Sensors = list(MetaData["Sensor_ID"])
         else:
             Sensors = Selections
+        count=0
         for i in Sensors:
+            print(str(count)+" de "+ str(len(Sensors)))
             x = self.GetEitRawData(Building=Building, Selections=[i], DateStart=DateStart, DateEnd=DateEnd,
                                    Resample=True)
+            count = count+1
             if len(x) != 0:
                 x = x.loc[:, ["value", "date"]]
                 from dateutil import parser
@@ -163,17 +216,14 @@ def Timestamp(date):
     # x=date.strftime('%s')
     # date = datetime.datetime(int(year), int(moth), int(day), int(hour), int(minu), int(seclist[0]), int(0), pytz.UTC)
     return (date - datetime.datetime(1970, 1, 1, 0, 0, 0, 0, pytz.UTC)).total_seconds()
-
-
 def Date(t):
     return datetime.datetime.fromtimestamp(t)
-
 
 if __name__ == '__main__':
     print("Star")
     x = DRepositoryAPI("cjferba", "alfaomega")
 
-    DateStart = "2017-06-16T00:00:00.000000Z"
+    DateStart = "2017-08-16T00:00:00.000000Z"
     DateEnd = "2017-08-17T00:00:00.000000Z"
     s = (x.GetEitData(Building="FaroBMS", DateStart=DateStart, DateEnd=DateEnd))#,Selections=[9003]))
     s.to_csv("DataRepository.csv")
